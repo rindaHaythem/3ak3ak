907126271e99036675d95bb3a0a7096fbb55cf83
TrabelsiHaythem <80393497+rindaHaythem@users.noreply.github.com>
2021-04-01 17:54:39 +0100

    Add project files.

---
+++ 3aka3ak/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _3aka3ak.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
